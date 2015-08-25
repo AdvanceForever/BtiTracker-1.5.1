c4b255dbe5a920361559a14d87597df0f1e5388a
Yupy <yupy_17y@yahoo.com>
2015-08-25 07:35:09 +0300

    Modified main_menu block for the new Theme

--- blocks/mainmenu_block.php
+++ blocks/mainmenu_block.php
@@ -1,5 +1,3 @@
-<table class='lista' width='100%'>
-<tr>
 <?php
 /*
 * BtiTracker v1.5.1 is a php tracker system for BitTorrent, easy to setup and configure.
@@ -7,46 +5,80 @@
 * Updated and Maintained by Yupy.
 * Copyright (C) 2004-2015 Btiteam.org
 */
+global $tpl, $STYLEPATH;
 
-if (!user::$current) {
-    // anonymous=guest
-    print("<td class='header' align='center'>" . WELCOME . " " . GUEST . "\n");
-    print("<a href='login.php'>(" . LOGIN . ")</a></td>");
-} elseif (user::$current["uid"] == 1)
-// anonymous=guest
-{
-    print("<td class='header' align='center'>" . WELCOME . " " . user::$current["username"] . " \n");
-    print("<a href='login.php'>(" . LOGIN . ")</a></td>\n");
+#Lang...
+$lang_welcome_guest = WELCOME . ' ' . GUEST;
+$tpl->assign('lang_welcome_guest', $lang_welcome_guest);
+
+$lang_login = LOGIN;
+$tpl->assign('lang_login', $lang_login);
+
+$lang_welcome = WELCOME;
+$tpl->assign('lang_welcome', $lang_welcome);
+
+$lang_welcome_back = WELCOME_BACK;
+$tpl->assign('lang_welcome_back', $lang_welcome_back);
+
+$lang_logout = LOGOUT;
+$tpl->assign('lang_logout', $lang_logout);
+
+$lang_index = MNU_INDEX;
+$tpl->assign('lang_index', $lang_index);
+
+$lang_torrents = MNU_TORRENT;
+$tpl->assign('lang_torrents', $lang_torrents);
+
+$lang_stats = MNU_STATS;
+$tpl->assign('lang_stats', $lang_stats);
+
+$lang_upload = MNU_UPLOAD;
+$tpl->assign('lang_upload', $lang_upload);
+
+$lang_members = MNU_MEMBERS;
+$tpl->assign('lang_members', $lang_members);
+
+$lang_news = MNU_NEWS;
+$tpl->assign('lang_news', $lang_news);
+
+$lang_forum = MNU_FORUM;
+$tpl->assign('lang_forum', $lang_forum);
+
+#Vars...
+$is_guest = (!user::$current);
+$tpl->assign('is_guest', $is_guest);
+
+$is_guest1 = (user::$current['uid'] == 1);
+$tpl->assign('is_guest1', $is_guest1);
+
+$cur_username = user::$current['username'];
+$tpl->assign('cur_username', $cur_username);
+
+$logout_salt = md5("R45eOMs15mNd3yV" . user::$current['username']);
+$tpl->assign('logout_salt', $logout_salt);
+
+$view_torrents = (user::$current['view_torrents'] == 'yes');
+$tpl->assign('view_torrents', $view_torrents);
+
+$can_upload = (user::$current['can_upload'] == 'yes');
+$tpl->assign('can_upload', $can_upload);
+
+$view_users = (user::$current['view_users'] == 'yes');
+$tpl->assign('view_users', $view_users);
+
+$view_news = (user::$current['view_news'] == 'yes');
+$tpl->assign('view_news', $view_news);
+
+$view_forum = (user::$current['view_forum'] == 'yes');
+$tpl->assign('view_forum', $view_forum);
+
+#Draw...
+if (user::$current['style'] == 1) {
+    $main_menu = $tpl->draw($STYLEPATH . '/tpl/header', $return_string = true);
+    echo $main_menu;
 } else {
-    print("<td class='header' align='center'>" . WELCOME_BACK . " " . user::$current["username"] . " \n");
-    
-	$salty = md5("R45eOMs15mNd3yV" . user::$current['username']); 
-    print("<a href='logout.php?check_hash=" . $salty . "'>(" . LOGOUT . ")</a></td>\n");
-}
-
-print("<td class='header' align='center'><a href='./'>" . MNU_INDEX . "</a></td>\n");
-
-if (user::$current["view_torrents"] == "yes") {
-    print("<td class='header' align='center'><a href='torrents.php'>" . MNU_TORRENT . "</a></td>\n");
-    print("<td class='header' align='center'><a href='extra-stats.php'>" . MNU_STATS . "</a></td>\n");
-}
-
-if (user::$current["can_upload"] == "yes")
-    print("<td class='header' align='center'><a href='upload.php'>" . MNU_UPLOAD . "</a></td>\n");
-
-if (user::$current["view_users"] == "yes")
-    print("<td class='header' align='center'><a href='users.php'>" . MNU_MEMBERS . "</a></td>\n");
-
-if (user::$current["view_news"] == "yes")
-    print("<td class='header' align='center'><a href='viewnews.php'>" . MNU_NEWS . "</a></td>\n");
-
-if (user::$current["view_forum"] == "yes") {
-    if ($GLOBALS["FORUMLINK"] == "" || $GLOBALS["FORUMLINK"] == "internal")
-        print("<td class='header' align='center'><a href='forum.php'>" . MNU_FORUM . "</a></td>\n");
-    else
-        print("<td class='header' align='center'><a href='" . $GLOBALS['FORUMLINK'] . "' target='_blank'>" . MNU_FORUM . "</a></td>\n");
+    $main_menu = $tpl->draw($STYLEPATH . '/tpl/main_menu', $return_string = true);
+    echo $main_menu;
 }
 
 ?>
-</tr>
-</table>
