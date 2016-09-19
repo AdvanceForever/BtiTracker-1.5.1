6d908ca2de72f6d58019413133f96a7c057e9860
Yupy <yupy_17y@yahoo.com>
2016-09-19 09:57:51 +0300

    Smarty Conversion

--- requests.php
+++ requests.php
@@ -10,82 +10,61 @@ require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEP
 dbconn();
 
 standardheader('Requests');
-begin_frame(MAKE_REQUEST );
 
-print("<br />\n");
+begin_frame(MAKE_REQUEST);
 
 $where = "WHERE userid = " . user::$current['uid'];
 $res2 = $db->query("SELECT * FROM requests " . $where) or sqlerr();
 $num2 = $res2->num_rows;
 
-?>
+$smarty->assign('lang_search', SEARCH);
+$smarty->assign('lang_torrent', TORRENT);
+$smarty->assign('lang_add_requests', ADD_REQUESTS);
+$smarty->assign('lang_torrent_file', TORRENT_FILE);
+$smarty->assign('lang_category', CATEGORY);
+$smarty->assign('lang_description', DESCRIPTION);
+$smarty->assign('lang_confirm', FRM_CONFIRM);
 
-<table align='center' border='0' width='100%' cellspacing='0' cellpadding='3'>
-<tr>
-   <td class='header' align='center'><?php print(SEARCH . " " . TORRENT); ?></td>
-</tr>
-<tr>
-   <td align='center' class='lista'><form method='get' action='torrents.php'>
-   <input type='text' name="<?php print('search'); ?>" size='40' value="<?php echo security::html_safe($searchstr) ?>" />
-   in
-   <select name='category'>
-   <option value='0'>(Select)</option>
+$smarty->assign('search_string', security::html_safe($searchstr));
 
-   <?php
+$cats = Cached::genrelist();
+$catdropdown = '';
 
-   $cats = Cached::genrelist();
-   $catdropdown = '';
-   foreach ($cats as $cat) {
-            $catdropdown .= "<option value='" . (int)$cat['id'] . "'";
-            if ($cat['id'] == $_GET['cat'])
-                $catdropdown .= " selected='selected'";
-            $catdropdown .= ">" . security::html_safe($cat['name']) . "</option>\n";
-   }
+foreach ($cats as $cat) {
+         $catdropdown .= "<option value='" . (int)$cat['id'] . "'";
+         if ($cat['id'] == $_GET['cat'])
+             $catdropdown .= " selected='selected'";
 
-   $deadchkbox = "<input type='checkbox' name='active' value='0'";
-   if ($_GET['active'])
-       $deadchkbox .= " checked='checked'";
-   $deadchkbox .= " /> " . INC_DEAD . "\n";
+         $catdropdown .= ">" . security::html_safe($cat['name']) . "</option>\n";
+}
 
-   echo $catdropdown;
-   print('</select>');
-   echo $deadchkbox;
-   print("<input type='submit' value='" . SEARCH . "' />
-   </form>
-   </td></tr></table><br><hr>");
+$deadchkbox = "<input type='checkbox' name='active' value='0'";
 
-   print("<br>\n");
-   print("<table class='lista' align='center' width='550' cellspacing='2'><form name='request' method='post' action='takerequest.php'><a name='add' id='add'></a>");
-   print("<tr><td class='header' align='center' width='100%' colspan='2'>" . ADD_REQUESTS . "</td></tr>");
-   print("<tr><td class='header' align='left' width='30%'>". TORRENT_FILE . "</td><td class='lista' align='left' width='70%'><input type='text' size='40' name='requesttitle'></td></tr>");
-   print("<tr><td class='header' align='left' width='30%'>" . CATEGORY . "</td><td class='lista' align='left' width='70%'>");
+if ($_GET['active'])
+    $deadchkbox .= " checked='checked'";
 
-   print("<select name='category'><option value='0'>(Select)</option>");
+$deadchkbox .= " /> " . INC_DEAD . "\n";
 
-   $res2 = $db->query("SELECT id, name FROM categories ORDER BY name");
-   $num = $res2->num_rows;
+$smarty->assign('category', $catdropdown);
+$smarty->assign('dead_checkbox', $deadchkbox);
 
-   $catdropdown2 = '';
-   for ($i = 0; $i < $num; ++$i) {
-        $cats2 = $res2->fetch_assoc();  
-        $catdropdown2 .= "<option value='" . (int)$cats2['id'] . "'";
-        $catdropdown2 .= ">" . security::html_safe($cats2['name']) . "</option>\n";
-   }
+$res2 = $db->query("SELECT id, name FROM categories ORDER BY name");
+$num = $res2->num_rows;
 
-   echo $catdropdown2;
+$catdropdown2 = '';
 
-   print("</select>");
+for ($i = 0; $i < $num; ++$i) {
+     $cats2 = $res2->fetch_assoc();  
+     $catdropdown2 .= "<option value='" . (int)$cats2['id'] . "'";
+     $catdropdown2 .= ">" . security::html_safe($cats2['name']) . "</option>\n";
+}
 
-   print("<br>\n");
+$smarty->assign('category2', $catdropdown2);
+$smarty->assign('description', textbbcode2('request', 'description'));
 
-   print("<tr><td class='header' align='left' width='30%'>" . DESCRIPTION . "</td><td class='lista' align='left' width='70%'>");
-   print(textbbcode('request', 'description'));
-   print("</td></tr>");
-   print("<tr><td class='lista' align='center' width='100%' colspan='2'><input type='submit' value='" . FRM_CONFIRM . "'></td></tr>");
-   print("</form>\n");
-   print("</table>\n");
+$smarty->display($STYLEPATH . '/tpl/tracker/make_request.tpl');
 
-   block_end();
-   stdfoot();
+block_end();
+stdfoot();
 
 ?>
