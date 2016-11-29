f8916b127a9c91208b40680d282b56338b5fec3e
Yupy <yupy_17y@yahoo.com>
2016-11-30 00:56:50 +0200

    Smarty Conversion...

--- delete.php
+++ delete.php
@@ -11,39 +11,43 @@ dbconn();
 
 standardheader('Delete Torrents');
 
-$id = $db->real_escape_string($_GET["info_hash"]);
+$id = $db->real_escape_string($_GET['info_hash']);
 
 if (!isset($id) || !$id)
-    die("Error ID");
+    die('Error ID');
 
-$res = $db->query("SELECT namemap.info_hash, namemap.uploader, namemap.filename, namemap.url, UNIX_TIMESTAMP(namemap.data) AS data, namemap.size, namemap.comment, categories.name AS cat_name, summary.seeds, summary.leechers, summary.finished, summary.speed FROM namemap LEFT JOIN categories ON categories.id = namemap.category LEFT JOIN summary ON summary.info_hash = namemap.info_hash WHERE namemap.info_hash = '" . $id . "'");
-$row = $res->fetch_array(MYSQLI_BOTH);
+$row = MCached::get('delete::query::' . $id);
+if ($row === MCached::NO_RESULT) {
+    $res = $db->query("SELECT namemap.info_hash, namemap.uploader, namemap.filename, namemap.url, UNIX_TIMESTAMP(namemap.data) AS data, namemap.size, namemap.comment, categories.name AS cat_name, summary.seeds, summary.leechers, summary.finished, summary.speed FROM namemap LEFT JOIN categories ON categories.id = namemap.category LEFT JOIN summary ON summary.info_hash = namemap.info_hash WHERE namemap.info_hash = '" . $id . "'");
+    $row = $res->fetch_array(MYSQLI_BOTH);
+    MCached::add('delete::query::' . $id, $row, 1800);
+}
 
-if (user::$current["delete_torrents"] != "yes" && user::$current["uid"] != $row["uploader"]) {
+if (user::$current['delete_torrents'] != 'yes' && user::$current['uid'] != $row['uploader']) {
     err_msg(SORRY, CANT_DELETE_TORRENT);
     stdfoot();
     exit();
 }
 
-$scriptname = security::html_safe($_SERVER["PHP_SELF"]);
+$scriptname = security::esc_url($_SERVER['PHP_SELF']);
 
-$link = urlencode($_GET["returnto"]);
-$hash = AddSlashes($_GET["info_hash"]);
+$link = urlencode($_GET['returnto']);
+$hash = AddSlashes($_GET['info_hash']);
 
-if ($link == "")
-    $link = "torrents.php";
+if ($link == '')
+    $link = 'torrents.php';
 
-if (isset($_POST["action"])) {
-    if ($_POST["action"] == DELETE) {
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == DELETE) {
         $ris = $db->query("SELECT info_hash, filename, url FROM namemap WHERE info_hash = '" . $hash . "'");
         if ($ris->num_rows == 0) {
-            err_msg("Sorry!", "Torrent " . $hash . " not found.");
+            err_msg('Sorry!', 'Torrent ' . $hash . ' not found.');
             exit();
         } else {
             list($torhash, $torname, $torurl) = $ris->fetch_array(MYSQLI_BOTH);
         }
 
-        write_log("Deleted torrent " . $torname . " (" . $torhash . ")", "delete");
+        write_log('Deleted torrent ' . $torname . ' (' . $torhash . ')', 'delete');
         
         @$db->query("DELETE FROM summary WHERE info_hash = '" . $hash . "'");
         @$db->query("DELETE FROM namemap WHERE info_hash = '" . $hash . "'");
@@ -56,6 +60,7 @@ if (isset($_POST["action"])) {
         MCached::del('torrent::details::' . $hash);
         MCached::del('torrent::details::image::' . $hash);
         MCached::del('torrent::details::nuked::requested::' . $hash);
+        MCached::del('delete::query::' . $hash);
         
         unlink($TORRENTSDIR . "/" . $hash . ".btf");
         
@@ -69,36 +74,53 @@ if (isset($_POST["action"])) {
 
 block_begin(DELETE_TORRENT);
 
-print("<table width='100%' class='lista' border='0' cellspacing='5' cellpadding='5'>\n");
-print("<tr><td align='right' class='header'>" . FILE_NAME . ":</td><td class='lista'>" . security::html_safe($row["filename"]) . "</td></tr>");
-print("<tr><td align='right' class='header'>" . INFO_HASH . ":</td><td class='lista'>" . security::html_safe($row["info_hash"]) . "</td></tr>");
+#Lang...
+$smarty->assign('lang',
+                 array('file_name' => FILE_NAME,
+                       'info_hash' => INFO_HASH,
+                       'description' => DESCRIPTION,
+                       'category' => CATEGORY_FULL,
+                       'size' => SIZE,
+                       'added' => ADDED,
+                       'speed' => SPEED,
+                       'downloaded' => DOWNLOADED,
+                       'peers' => PEERS,
+                       'seeders' => SEEDERS,
+                       'leechers' => LEECHERS,
+                       'delete' => DELETE,
+                       'cancel' => FRM_CANCEL
+                      )
+               );
 
-if (!empty($row["comment"]))
-    print("<tr><td align='right' class='header'>" . DESCRIPTION . ":</td><td align='left' class='lista'>" . format_comment(unesc($row["comment"])) . "</td></tr>");
+#Vars...
+$smarty->assign('filename', security::html_safe($row['filename']));
+$smarty->assign('info_hash', security::html_safe($row['info_hash']));
+$smarty->assign('description', format_comment(unesc($row['comment'])));
+$smarty->assign('category', security::html_safe($row['cat_name']));
+$smarty->assign('size', misc::makesize((int)$row['size']));
+$smarty->assign('added', date('d/m/Y H:m:s', $row['data']));
+$smarty->assign('finished', (int)$row['finished']);
+$smarty->assign('seeders', (int)$row['seeds']);
+$smarty->assign('leechers', (int)$row['leechers']);
+$smarty->assign('peers', ((int)$row['leechers'] + (int)$row['seeds']));
+$smarty->assign('scriptname', $scriptname);
+$smarty->assign('id', $id);
+$smarty->assign('link', $link);
 
-if (isset($row["cat_name"]))
-    print("<tr><td align='right' class='header'>" . CATEGORY_FULL . ":</td><td class='lista'>" . security::html_safe($row["cat_name"]) . "</td></tr>");
-else
-    print("<tr><td align='right' class='header'>" . CATEGORY_FULL . ":</td><td class='lista'>(None)</td></tr>");
+#If's...
+$smarty->assign('has_description', (!empty($row['comment'])));
+$smarty->assign('has_category', (isset($row['cat_name'])));
 
-print("<tr><td align='right' class='header'>" . SIZE . ":</td><td class='lista'>" . misc::makesize((int)$row["size"]) . "</td></tr>");
-print("<tr><td align='right' class='header'>" . ADDED . ":</td><td class='lista'>" . date("d/m/Y H:m:s", $row["data"]) . "</td></tr>");
-
-if ($row["speed"] < 0) {
-    $speed = "N/A";
-} else if ($row["speed"] > 2097152) {
-    $speed = round((int)$row["speed"] / 1048576, 2) . " MiB per sec";
+if ($row['speed'] < 0) {
+    $speed = 'N/A';
+} else if ($row['speed'] > 2097152) {
+    $speed = round((int)$row['speed'] / 1048576, 2) . ' MiB per sec';
 } else {
-    $speed = round((int)$row["speed"] / 1024, 2) . " KiB per sec";
+    $speed = round((int)$row['speed'] / 1024, 2) . ' KiB per sec';
 }
-print("<tr><td align='right' class='header'>" . SPEED . ":</td><td class='lista'>" . $speed . "</td></tr>");
-print("<tr><td align='right' class='header'>" . DOWNLOADED . ":</td><td class='lista'>" . (int)$row["finished"] . "</td></tr>");
-print("<tr><td align='right' class='header'>" . PEERS . ":</td><td class='lista'>" . SEEDERS . ": " . (int)$row["seeds"] . ", " . LEECHERS . ": " . (int)$row["leechers"] . " = " . ((int)$row["leechers"] + (int)$row["seeds"]) . " " . PEERS . "</td></tr>");
-print("</table>\n");
-print("<form action='" . $scriptname . "?info_hash=" . $id . "&returnto=" . $link . "' name='delete' method='post'>");
-print("<center><input type='submit' name='action' value='" . DELETE . "' />");
-print("&nbsp;&nbsp;<input type='submit' name='action' value='" . FRM_CANCEL . "' /></center>");
-print("</form>");
+$smarty->assign('speed', $speed);
+
+$smarty->display($STYLEPATH . '/tpl/torrent/delete.tpl');
 
 block_end();
 stdfoot();
