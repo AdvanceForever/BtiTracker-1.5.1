76c8d4fc47dec8691e413545a662f38969eb2bf0
Yupy <yupy17y@live.com>
2020-11-08 02:12:07 +0200

    Cached Dir.

--- include/classes/Smarty.class.php
+++ include/classes/Smarty.class.php
@@ -629,7 +629,7 @@ class Smarty extends Smarty_Internal_TemplateBase
      *
      * @var string
      */
-    protected $compile_dir = './templates_c/';
+    protected $compile_dir = './cache/';
 
     /**
      * plugins directory
