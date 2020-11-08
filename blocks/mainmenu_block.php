001fe52ee2e01241fefb7ead47381bd5a31cc715
Yupy <yupy17y@live.com>
2020-11-08 03:26:30 +0200

    Migrate old RainTPL code to Smarty

--- blocks/mainmenu_block.php
+++ blocks/mainmenu_block.php
@@ -3,81 +3,43 @@
 * BtiTracker v1.5.1 is a php tracker system for BitTorrent, easy to setup and configure.
 * This tracker is a frontend for DeHackEd's tracker, aka phpBTTracker (now heavely modified). 
 * Updated and Maintained by Yupy.
-* Copyright (C) 2004-2015 Btiteam.org
+* Copyright (C) 2004-2020 Btiteam.org
 */
-global $tpl, $STYLEPATH;
+global $smarty, $STYLEPATH;
 
 #Lang...
-$lang_welcome_guest = WELCOME . ' ' . GUEST;
-$tpl->assign('lang_welcome_guest', $lang_welcome_guest);
-
-$lang_login = LOGIN;
-$tpl->assign('lang_login', $lang_login);
-
-$lang_welcome = WELCOME;
-$tpl->assign('lang_welcome', $lang_welcome);
-
-$lang_welcome_back = WELCOME_BACK;
-$tpl->assign('lang_welcome_back', $lang_welcome_back);
-
-$lang_logout = LOGOUT;
-$tpl->assign('lang_logout', $lang_logout);
-
-$lang_index = MNU_INDEX;
-$tpl->assign('lang_index', $lang_index);
-
-$lang_torrents = MNU_TORRENT;
-$tpl->assign('lang_torrents', $lang_torrents);
-
-$lang_stats = MNU_STATS;
-$tpl->assign('lang_stats', $lang_stats);
-
-$lang_upload = MNU_UPLOAD;
-$tpl->assign('lang_upload', $lang_upload);
-
-$lang_members = MNU_MEMBERS;
-$tpl->assign('lang_members', $lang_members);
-
-$lang_news = MNU_NEWS;
-$tpl->assign('lang_news', $lang_news);
-
-$lang_forum = MNU_FORUM;
-$tpl->assign('lang_forum', $lang_forum);
+$smarty->assign('lang_welcome_guest', WELCOME . ' ' . GUEST);
+$smarty->assign('lang_login', LOGIN);
+$smarty->assign('lang_welcome', WELCOME);
+$smarty->assign('lang_welcome_back', WELCOME_BACK);
+$smarty->assign('lang_logout', LOGOUT);
+$smarty->assign('lang_index', MNU_INDEX);
+$smarty->assign('lang_torrents', MNU_TORRENT);
+$smarty->assign('lang_stats', MNU_STATS);
+$smarty->assign('lang_upload', MNU_UPLOAD);
+$smarty->assign('lang_members', MNU_MEMBERS);
+$smarty->assign('lang_news', MNU_NEWS);
+$smarty->assign('lang_forum', MNU_FORUM);
 
 #Vars...
-$is_guest = (!user::$current);
-$tpl->assign('is_guest', $is_guest);
-
-$is_guest1 = (user::$current['uid'] == 1);
-$tpl->assign('is_guest1', $is_guest1);
-
-$cur_username = user::$current['prefixcolor'] . user::$current['username'] . user::$current['suffixcolor'];
-$tpl->assign('cur_username', $cur_username);
-
-$logout_salt = md5("R45eOMs15mNd3yV" . user::$current['username']);
-$tpl->assign('logout_salt', $logout_salt);
-
-$view_torrents = (user::$current['view_torrents'] == 'yes');
-$tpl->assign('view_torrents', $view_torrents);
-
-$can_upload = (user::$current['can_upload'] == 'yes');
-$tpl->assign('can_upload', $can_upload);
-
-$view_users = (user::$current['view_users'] == 'yes');
-$tpl->assign('view_users', $view_users);
-
-$view_news = (user::$current['view_news'] == 'yes');
-$tpl->assign('view_news', $view_news);
+$smarty->assign('is_guest', (!user::$current));
+$smarty->assign('is_guest1', (user::$current['uid'] == 1));
+$smarty->assign('cur_username', user::$current['prefixcolor'] . user::$current['username'] . user::$current['suffixcolor']);
+$smarty->assign('logout_salt', md5("R45eOMs15mNd3yV" . user::$current['username']));
+$smarty->assign('view_torrents', (user::$current['view_torrents'] == 'yes'));
+$smarty->assign('can_upload', (user::$current['can_upload'] == 'yes'));
+$smarty->assign('view_users', (user::$current['view_users'] == 'yes'));
+$smarty->assign('view_news', (user::$current['view_news'] == 'yes'));
 
 $view_forum = (user::$current['view_forum'] == 'yes');
-$tpl->assign('view_forum', $view_forum);
+$smarty->assign('view_forum', (user::$current['view_forum'] == 'yes'));
 
 #Draw...
 if (user::$current['style'] == 1) {
-    $main_menu = $tpl->draw($STYLEPATH . '/tpl/header', $return_string = true);
+    $main_menu = $smarty->display($STYLEPATH . '/tpl/header.tpl', $return_string = true);
     echo $main_menu;
 } else {
-    $main_menu = $tpl->draw($STYLEPATH . '/tpl/main_menu', $return_string = true);
+    $main_menu = $smarty->display($STYLEPATH . '/tpl/main_menu.tpl', $return_string = true);
     echo $main_menu;
 }
 
